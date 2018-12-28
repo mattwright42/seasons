2ce4750cb6837c58871d3a74d251f20eff33d043
Matthew Wright <matthewbwright@gmail.com>
2018-12-28 10:43:12 -0700

    adds geolocation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there!</div>
-}
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position), //the success callback
+    err => console.log(err)
+  );
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-)
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
